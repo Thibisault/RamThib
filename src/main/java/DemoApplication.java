cfc0f1cb723dd275c29af7711d864ac84cb42b21
Thibisault <thibault.danis@gmail.com>
2024-02-12 10:14:45 +0100

    Third

--- src/main/java/DemoApplication.java
+++ src/main/java/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+
 
 import com.example.demo.dto.CollectiviteDto;
 import com.example.demo.dto.FactureDto;
@@ -11,25 +11,14 @@ import com.example.demo.service.FactureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-@RestController
-@SpringBootApplication(scanBasePackages={
-        "package com.example.demo.dto.CollectiviteMapper", "package com.example.demo.dto.FactureMapper"})
+@SpringBootApplication
 public class DemoApplication {
-    
-    @GetMapping("/")
-    public String firstLa(){
-        return "Mister";
-    }
-    
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
